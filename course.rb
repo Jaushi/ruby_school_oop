11f60c1d4c05ed6e5a771c1844b95dcf70ddb835
Jaushi <joshpagallaman10@gmail.com>
2024-09-19 13:30:31 +0800

    Created the Course class

---
+++ course.rb
@@ -0,0 +1,29 @@
+class Course
+  attr_accessor :id, :name
+  @@record = []
+
+  def initialize(id, name)
+    @id = id
+    @name = name
+  end
+
+  def save
+    @@record.prepend(self)
+  end
+
+  def destroy
+    @@record.delete(self)
+  end
+
+  def display
+    puts "#{@id}: #{@name}"
+  end
+
+  def self.all
+    @@record
+  end
+
+  def self.find(id)
+    @@record.detect { |course| course.id == id }
+  end
+end
