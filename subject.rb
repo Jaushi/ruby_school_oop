02059bf2fa82a46fc3d4eff593b45e2becf1754c
Jaushi <joshpagallaman10@gmail.com>
2024-09-19 14:46:07 +0800

    Created the Subject class

---
+++ subject.rb
@@ -0,0 +1,29 @@
+class Subject
+  attr_accessor :id, :name
+  @@record = []
+
+  def initialize(id, name)
+    @id = id
+    @name = name
+  end
+
+  def save
+    @@record.prepend(self)
+  end
+
+  def destroy
+    @@record.delete(self)
+  end
+
+  def display
+    puts "#{@id}: #{@name}"
+  end
+
+  def self.all
+    @@record
+  end
+
+  def self.find(id)
+    @@record.detect { |subject| subject.id == id }
+  end
+end
