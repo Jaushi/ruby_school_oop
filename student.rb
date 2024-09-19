20d948814808f6b1d85443b1f8d154a886ead776
Jaushi <joshpagallaman10@gmail.com>
2024-09-19 11:05:19 +0800

    Created the Student class

---
+++ student.rb
@@ -0,0 +1,36 @@
+class Student
+  attr_accessor :id, :name, :birth_date, :email, :phone_number
+  @@record = []
+
+  def initialize(id, name, birth_date, email, phone_number)
+    @id = id
+    @name = name
+    @birth_date = birth_date
+    @email = email
+    @phone_number = phone_number
+  end
+
+  def save
+    @@record.prepend(self)
+  end
+
+  def destroy
+    @@record.delete(self)
+  end
+
+  def display
+    puts "#{@id}, #{@name}, #{@birth_date}, #{@email}, #{@phone_number}"
+  end
+
+  def self.all
+    @@record
+  end
+
+  def self.find(id)
+    @@record.detect { |student| student.id == id }
+  end
+
+  def self.find_by_email(email)
+    @@record.detect { |student| student.email == email }
+  end
+end
