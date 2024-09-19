0f7bfcac0042f49b4776489910cf3875ea91e875
Jaushi <joshpagallaman10@gmail.com>
2024-09-19 14:44:00 +0800

    Modified: Added an add and delete method for course management and created a program choice to run until the user exits

--- main.rb
+++ main.rb
@@ -1,9 +1,9 @@
 require_relative 'student'
+require_relative 'course'
 
 def add_student
   puts "|ADD STUDENT|"
-  print "Enter Student ID:"
-  add_id = gets.chomp
+  add_id = Student.all.size + 1
   print "Enter Student Name:"
   add_name = gets.chomp
   print "Enter Student Birth date 'mm/dd/yy':"
@@ -26,7 +26,7 @@ end
 
 def remove_student
   print "Enter Student ID to delete:"
-  destroy_student = gets.chomp
+  destroy_student = gets.chomp.to_i
 
   student = Student.find(destroy_student)
   if student
@@ -40,30 +40,87 @@ def remove_student
   end
 end
 
+def add_course
+  puts "|ADD COURSE|"
+  add_course_id = Course.all.size + 1
+  print "Enter Course Name:"
+  add_course_name = gets.chomp
+  course = Course.new(add_course_id, add_course_name)
+  course.save
+
+  if Course.find(add_course_id)
+    puts "Course added successfully!"
+    puts course.display
+  else
+    puts "Failed to add student."
+  end
+end
+
+def remove_course
+  print "Enter Course ID to delete:"
+  destroy_course = gets.chomp.to_i
+
+  student = Course.find(destroy_course)
+  if student
+    if student.destroy
+      puts "Course destroyed successfully!"
+    else
+      puts "Failed to destroy from the record."
+    end
+  else
+    puts "Course not found."
+  end
+end
+
 def quit_program
   puts "Exiting..."
   exit
 end
 
-def display_menu
-  puts "[a] Add a Student"
-  puts "[d] Delete a Student"
-  puts "[e] Exit Program"
+def student_management
+  puts "[1] Add Student"
+  puts "[2] Delete Student"
   print "Enter an Action to do: "
+  choice = gets.chomp.to_i
+
+  if choice == 1
+    add_student
+  end
+  if choice == 2
+    remove_student
+  end
 end
 
-choice = nil
-while choice != 'e'
-  display_menu
-  choice = gets.chomp.downcase
-  case choice
-  when 'a'
-    add_student
-  when 'd'
-    remove_student
-  when 'e'
+def course_management
+  puts "[1] Add Course"
+  puts "[2] Delete Course"
+  print "Enter an Action to do: "
+  choice = gets.chomp.to_i
+
+  if choice == 1
+    add_course
+  end
+  if choice == 2
+    remove_course
+  end
+end
+
+program_choice = nil
+while program_choice != 0
+  puts "[1] Student Management"
+  puts "[2] Course Management"
+  puts "[0] Exit Program"
+  print "Enter a Program to Access: "
+  program_choice = gets.chomp.to_i
+
+  case program_choice
+  when 1
+    student_management
+  when 2
+    course_management
+  when 0
     quit_program
   else
-    puts "Invalid choice. Enter 'a', 'd', 'e'."
+    puts "Invalid choice. Enter 1, 2, 0."
   end
 end
